db8941013c0f2a7e1ff36e5da72c2a896c1d6b71
BertoGz <AlbertoG@albertos-mbp.attlocal.net>
2020-06-03 16:41:10 -0700

    data is now stored in the store

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,14 +1,14 @@
 import {getInitialData} from '../utils/api'
 
 //import action creators
-import {receiveUsersAction} from './actions/users'
-import {receiveQuestionsAction} from './actions/questions'
-import {setAuthedUserAction} from './actions/authedUser'
+import {receiveUsersAction} from '../actions/users'
+import {receiveQuestionsAction} from '../actions/questions'
+import {setAuthedUserAction} from '../actions/authedUser'
 
 const AUTHED_ID = 'tylermcginnis'
 export function handleInitialData (){
 	return(dispatch)=>{
-		return getInitialData().then((users,questions)=>{
+		return getInitialData().then(({users,questions})=>{
 			dispatch(receiveUsersAction(users))
 			dispatch(receiveQuestionsAction(questions))
 			dispatch(setAuthedUserAction(AUTHED_ID))
